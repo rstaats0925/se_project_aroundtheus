842043ba00d3c1ee4d79cbf3077fb4d93f444c7c
Ryan Staats <rstaats0927@gmail.com>
2023-03-25 12:25:04 -0500

    Added js file

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let initialCards = [
+  {
+    name: "Grand Canyon",
+    link: "https://unsplash.com/photos/ZZnH4GOzDgc"
+  },
+  
+  {
+    name: "Roman Coliseum",
+    link: "https://unsplash.com/photos/ybtNz4PeyPI"
+  },
+  
+  {
+    name: "Hard Rock Cafe",
+    link: "https://unsplash.com/photos/W1pewWc6e9E"
+  },
+  
+  {
+    name: "African Safari",
+    link: "https://unsplash.com/photos/OFbws4f_Ya8"
+  },
+  
+  {
+    name: "Glacier Bay National Park",
+    link: "https://unsplash.com/photos/gjD66bFxpKE"
+  },
+  
+  {
+    name: "Statue of Liberty",
+    link: "https://unsplash.com/photos/sf8b4ucpdkg"
+  } 
+]
