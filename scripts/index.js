91f37b014bb8d974f587502a03cfbaff421628a2
Ryan Staats <rstaats0927@gmail.com>
2023-04-04 18:51:42 -0500

    update: replaced hard coded cards with template

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,32 @@
 let initialCards = [ 
   {
     name: "Grand Canyon",
-    link: "https://unsplash.com/photos/ZZnH4GOzDgc"
+    link: "https://images.unsplash.com/photo-1615551043360-33de8b5f410c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80"
   },
   
   {
     name: "Roman Coliseum",
-    link: "https://unsplash.com/photos/ybtNz4PeyPI"
+    link: "https://images.unsplash.com/photo-1651984302679-2b247d0c9d7c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
   },
   
   {
     name: "Hard Rock Cafe",
-    link: "https://unsplash.com/photos/W1pewWc6e9E"
+    link: "https://images.unsplash.com/photo-1519617535550-82004e4b8206?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80"
   },
   
   {
     name: "African Safari",
-    link: "https://unsplash.com/photos/OFbws4f_Ya8"
+    link: "https://images.unsplash.com/photo-1534995385387-824e51d1c6a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=914&q=80"
   },
   
   {
     name: "Glacier Bay National Park",
-    link: "https://unsplash.com/photos/gjD66bFxpKE"
+    link: "https://images.unsplash.com/photo-1445196849952-1a17bdbb57b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=879&q=80"
   },
   
   {
     name: "Statue of Liberty",
-    link: "https://unsplash.com/photos/sf8b4ucpdkg"
+    link: "https://images.unsplash.com/photo-1605130284535-11dd9eedc58a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
   } 
 ]
 
@@ -39,6 +39,7 @@ const modalInputUsername = document.querySelector("#modal-input-username");
 const profileSubtext = document.querySelector('.profile__subtext');
 const modalInputSubtext = document.querySelector("#modal-input-subtext");
 const modalEditForm = profileEditModal.querySelector(".modal__form");
+// const cardElement = document.querySelector("#card-template").content.firstElementChild;
 
 function applyFormInputValues(event) {
   event.preventDefault();
@@ -51,6 +52,22 @@ function toggleProfileEditModal(event) {
   modalInputUsername.value = profileUsername.textContent;
   modalInputSubtext.value = profileSubtext.textContent;
   profileEditModal.classList.toggle("modal__open");
+}
+
+function getCardElement(data) {
+  const cardTemplate = document.querySelector("#card-template").content.firstElementChild;
+  let cardElement = cardTemplate.cloneNode(true);
+  let cardImage = cardElement.querySelector(".card__image");
+  let cardCaption = cardElement.querySelector(".card__caption");
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardCaption.textContent = data.name;
+  return cardElement;
+}
+
+for (card of initialCards) {
+  const cardsGrid = document.querySelector(".cards__grid");
+  cardsGrid.append(getCardElement(card));
 }
 
 profileEditButton.addEventListener("click", toggleProfileEditModal);
