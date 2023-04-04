de660590096b581af57415bde529f041fe3de01f
Ryan Staats <rstaats0927@gmail.com>
2023-04-04 15:51:57 -0500

    update: modal can edit profile info

--- scripts/index.js
+++ scripts/index.js
@@ -30,13 +30,29 @@ let initialCards = [
   } 
 ]
 
+//elements
 const profileEditButton = document.querySelector("#profile-edit-btn");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const modalCloseButton = document.querySelector("#modal-close-button");
+const profileUsername = document.querySelector(".profile__user-name");
+const modalInputUsername = document.querySelector("#modal-input-username");
+const profileSubtext = document.querySelector('.profile__subtext');
+const modalInputSubtext = document.querySelector("#modal-input-subtext");
+const modalEditForm = profileEditModal.querySelector(".modal__form");
 
-function toggleProfileEditModal() {
+function applyFormInputValues(event) {
+  event.preventDefault();
+  profileUsername.textContent = modalInputUsername.value;
+  profileSubtext.textContent = modalInputSubtext.value;
+  profileEditModal.classList.remove("modal__open");
+}
+
+function toggleProfileEditModal(event) {
+  modalInputUsername.value = profileUsername.textContent;
+  modalInputSubtext.value = profileSubtext.textContent;
   profileEditModal.classList.toggle("modal__open");
 }
 
 profileEditButton.addEventListener("click", toggleProfileEditModal);
 modalCloseButton.addEventListener("click", toggleProfileEditModal);
+modalEditForm.addEventListener("submit", applyFormInputValues);
