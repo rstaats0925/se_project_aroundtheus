5c60e27b965cbc130a5d2a34cc4b337b5b9bbdb2
Ryan Staats <rstaats0927@gmail.com>
2023-06-06 16:26:58 -0500

    started PopupWithFormClass

---
+++ components/PopupWithForm.js
@@ -0,0 +1,19 @@
+import Popup from "./Popup";
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, submitHandler) {
+
+  }
+
+  #getInputValues() {
+
+  }
+
+  setEventListeners() {
+
+  }
+
+  close() {
+
+  }
+}
