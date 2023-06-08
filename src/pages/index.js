91c0fc7ab342273625e70c6f6ce8f11aad36533e
Ryan Staats <rstaats0927@gmail.com>
2023-06-07 20:13:20 -0500

    update config files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
+import "./index.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
-import initialCards from "../blocks/utils/constants.js";
+import initialCards from "../utils/constants.js";
 import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import UserInfo from "../components/UserInfo.js";
