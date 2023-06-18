43215a2fd35c6e8ee7f2288f96d55ef4fb81ddc8
Ryan Staats <rstaats0927@gmail.com>
2023-06-18 18:34:20 -0500

    added methods to api class

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,3 +1,18 @@
+//https://around.nomoreparties.co/vi/{group-id}
+
 export default class Api {
-  
+  constructor(options) {
+    this.options = options;
+  }
+
+  getUserInfo () {
+    return fetch("https://around.nomoreparties.co/v1/groupId/users/me", {
+      authorization: ""
+    })
+    .then(response => response.json())
+  }
+
+  getInitialCards() {
+
+  }
 }
